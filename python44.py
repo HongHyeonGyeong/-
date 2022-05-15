c20120655cc1f277938162083542ae79445c8b07
HongHyeonGyeong <104764634+HongHyeonGyeong@users.noreply.github.com>
2022-05-15 22:13:53 +0900

    Add files via upload

---
+++ python44.py
@@ -0,0 +1,15 @@
+
+for i in range(0,4):
+    name = input()
+    score = int(input())
+
+    if range(90,101):
+        print("%s의 학점 : A" %name)
+    elif range(80,90):
+        print("%s의 학점 : B" %name)
+    elif range(70,80):
+        print("%s의 학점 : C" %name)
+    elif range(60,70):
+        print("%s의 학점 : D" %name)
+    else:
+        print("%s의 학점 : F" %name)
